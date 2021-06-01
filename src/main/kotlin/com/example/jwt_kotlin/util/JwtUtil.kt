fdd3aa8294e8a30800c3f6d04d9f2a3e9dce3eae
deniscozma2019 <vitaliesandiuc@gmail.com>
2021-06-01 13:34:07 +0300

    Converting from java code to Kotlin

--- src/main/kotlin/com/example/jwt_kotlin/util/JwtUtil.kt
+++ src/main/kotlin/com/example/jwt_kotlin/util/JwtUtil.kt
@@ -1,25 +1,52 @@
 package com.example.jwt_kotlin.util
 
-import io.jsonwebtoken.JwtBuilder
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
 import java.util.*
-import kotlin.collections.HashMap
+import java.util.function.Function
 
 @Service
 class JwtUtil {
     private val secret = "jwttry"
 
-    fun generateToken(username: String) :String {
+    fun generateToken(username: String): String? {
         val claims: Map<String, Any> = HashMap()
         return createToken(claims, username)
     }
 
-    private fun createToken(claims: Map<String, Any>, subject: String): String {
-        return Jwts.builder().setClaims(claims).setSubject(subject)
-            .setIssuedAt(Date(System.currentTimeMillis()))
-            .setExpiration(Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+     fun createToken(claims: Map<String, Any>, subject: String): String? {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(
+            Date(System.currentTimeMillis())
+        ).setExpiration(Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
             .signWith(SignatureAlgorithm.HS256, secret).compact()
+    }
+
+    fun isTokenExpired(token: String?): Boolean? {
+        return extractExpiration(token).before(Date())
+    }
+
+    fun extractExpiration(token: String?): Date {
+        return extractClaim(token) { obj: Claims -> obj.expiration }
+    }
+
+    fun extractAllClaims(token: String?): Claims {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).body
+    }
+
+    fun validateToken(token: String?, userDetails: UserDetails): Boolean? {
+        val username = extractUsername(token)
+        return username == userDetails.username && !isTokenExpired(token)!!
+    }
+
+    fun extractUsername(token: String?): String {
+        return extractClaim(token) { obj: Claims -> obj.subject }
+    }
+
+    fun <T> extractClaim(token: String?, claimsResolver: Function<Claims, T>): T {
+        val claims = extractAllClaims(token)
+        return claimsResolver.apply(claims)
     }
 }
