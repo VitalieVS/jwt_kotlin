315850a22bc61d36ee8404050be62b1d68d18f32
deniscozma2019 <vitaliesandiuc@gmail.com>
2021-06-01 12:43:15 +0300

    Initial commit

---
+++ src/test/kotlin/com/example/jwt_kotlin/JwtKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jwt_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
