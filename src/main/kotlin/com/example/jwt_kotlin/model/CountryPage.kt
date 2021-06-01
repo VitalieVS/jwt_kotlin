e2e5aa58fadcb3826c11ad39fd6e99fefa1412ef
deniscozma2019 <vitaliesandiuc@gmail.com>
2021-06-01 18:26:31 +0300

    CountryPage done

--- src/main/kotlin/com/example/jwt_kotlin/model/CountryPage.kt
+++ src/main/kotlin/com/example/jwt_kotlin/model/CountryPage.kt
@@ -1,3 +1,12 @@
 package com.example.jwt_kotlin.model
 
-data class CountryPage()
+import lombok.Data
+import org.springframework.data.domain.Sort
+
+@Data
+data class CountryPage(
+    val pageNumber: Int = 0,
+    val pageSize: Int = 5,
+    val sortDirection: Sort.Direction = Sort.Direction.ASC,
+    val sortBy: String = "name"
+)
