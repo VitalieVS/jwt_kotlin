62df5758b03420e651cbb261b6ff91ba71fa22d2
deniscozma2019 <vitaliesandiuc@gmail.com>
2021-06-01 12:55:12 +0300

    Security config created TODO: add implementation

--- src/main/kotlin/com/example/jwt_kotlin/repository/UserRepository.kt
+++ src/main/kotlin/com/example/jwt_kotlin/repository/UserRepository.kt
@@ -1,4 +1,4 @@
-package com.example.jwt_kotlin.repository.security
+package com.example.jwt_kotlin.repository
 
 import com.example.jwt_kotlin.User.User
 import org.springframework.data.jpa.repository.JpaRepository
---
+++ src/main/kotlin/com/example/jwt_kotlin/security/SecurityConfig.kt
@@ -0,0 +1,11 @@
+package com.example.jwt_kotlin.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig: WebSecurityConfigurerAdapter() {
+
+}
