61ed51186f08c6026acdf7b57619f6ebf7cf4c7f
deniscozma2019 <vitaliesandiuc@gmail.com>
2021-06-07 13:12:22 +0300

    Renamed City Controller

--- src/main/kotlin/com/example/jwt_kotlin/controller/CityController.kt
+++ src/main/kotlin/com/example/jwt_kotlin/controller/CityController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class RegionController {
+class CityController {
 
     @Autowired
     lateinit var regionService: RegionService
