cf950f66071da4ffad64235b9debd60e2d3d0d32
deniscozma2019 <vitaliesandiuc@gmail.com>
2021-06-07 17:25:18 +0300

    V14 PERMISSIONS

---
+++ src/main/resources/db/migration/V14__ADD__ROLES__PERMISSION.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles_permissions (role_id, permission_id) VALUES (10, 13);
+INSERT INTO roles_permissions (role_id, permission_id) VALUES (10, 14);
+INSERT INTO roles_permissions (role_id, permission_id) VALUES (10, 15);
+INSERT INTO roles_permissions (role_id, permission_id) VALUES (11, 13);
